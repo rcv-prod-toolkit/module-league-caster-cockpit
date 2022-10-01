0ebd063504bb8dc8cff4b0a36503760b37e637bc
Himyu <max_b98@hotmail.de>
2022-10-01 18:22:33 +0200

    update lobby

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,33 +1,34 @@
-const blueTeam = document.querySelector('.team.blue')
-const redTeam = document.querySelector('.team.red')
+const bluePlayers = document.querySelector('#bluePlayers')
+const redPlayers = document.querySelector('#redPlayers')
+const blueMultiLinkBtn = document.querySelector('#blueMultiLinkBtn')
+const redMultiLinkBtn = document.querySelector('#redMultiLinkBtn')
 
 function displayData(e) {
-  console.log(e)
-  const data = e.state?.lcu.lobby?.player ?? e.data
+  const data = e.state?.lcu.lobby ?? e.data
 
-  blueTeam.innerHTML = ''
-  redTeam.innerHTML = ''
+  bluePlayers.innerHTML = ''
+  redPlayers.innerHTML = ''
 
-  if (data === undefined) {
-    return
-  }
+  if (data === undefined) return
+  if (!data._available) return
 
   let blueMultiLink = 'https://euw.op.gg/multisearch/euw?summoners='
   let redMultiLink = 'https://euw.op.gg/multisearch/euw?summoners='
 
-  for (const player of data) {
-    const playerRow = document.createElement('div')
+  for (const player of data.members) {
+    const playerRow = document.createElement('li')
     playerRow.classList.add('playerRow')
+    playerRow.dataset.team = player.teamId
 
     const playerIcon = document.createElement('img')
     playerIcon.classList.add('playerIcon')
-    playerIcon.src = `/serve/module-league-static/img/profileicon/${player.icon}.png`
+    playerIcon.src = `/serve/module-league-static/img/profileicon/${player.summonerIconId}.png`
 
     playerRow.appendChild(playerIcon)
 
     const name = document.createElement('h3')
     name.classList.add('name')
-    name.innerText = player.name
+    name.innerText = player.summonerName
 
     const level = document.createElement('p')
     level.classList.add('level')
@@ -68,40 +69,20 @@ function displayData(e) {
 
     playerRow.appendChild(playerLink)
 
-    if (player.team === 100) {
+    if (player.teamId === 100) {
       blueMultiLink += player.name + ','
-      blueTeam.appendChild(playerRow)
+      bluePlayers.appendChild(playerRow)
     } else {
       redMultiLink += player.name + ','
-      redTeam.appendChild(playerRow)
+      redPlayers.appendChild(playerRow)
     }
   }
 
-  const blueMultiLinkBtn = document.createElement('a')
-  blueMultiLinkBtn.classList.add(
-    'multiLinkBtn',
-    'btn',
-    'btn-primary',
-    'btn-block'
-  )
   blueMultiLinkBtn.href = encodeURI(blueMultiLink.slice(0, -1))
-  blueMultiLinkBtn.target = '_blank'
-  blueMultiLinkBtn.innerHTML = 'OP.GG Overview'
-
-  blueTeam.appendChild(blueMultiLinkBtn)
-
-  const redMultiLinkBtn = document.createElement('a')
-  redMultiLinkBtn.classList.add(
-    'multiLinkBtn',
-    'btn',
-    'btn-primary',
-    'btn-block'
-  )
   redMultiLinkBtn.href = encodeURI(redMultiLink.slice(0, -1))
-  redMultiLinkBtn.target = '_blank'
-  redMultiLinkBtn.innerHTML = 'OP.GG Overview'
 
-  redTeam.appendChild(redMultiLinkBtn)
+  slist(bluePlayers)
+  slist(redPlayers)
 }
 
 async function initUi() {
@@ -114,6 +95,80 @@ async function initUi() {
   })
 
   displayData(data)
+}
+
+function slist(target) {
+  target.classList.add('slist')
+  const items = target.getElementsByTagName('li')
+  let current = null
+
+  for (const i of items) {
+    i.draggable = true
+
+    i.ondragstart = () => {
+      current = i
+      for (const it of items) {
+        if (it !== current) {
+          it.classList.add('hint')
+        }
+      }
+    }
+
+    i.ondragenter = () => {
+      if (i !== current) {
+        i.classList.add('active')
+      }
+    }
+
+    i.ondragleave = () => {
+      i.classList.remove('active')
+    }
+
+    i.ondragend = () => {
+      for (const it of items) {
+        it.classList.remove('hint')
+        it.classList.remove('active')
+      }
+    }
+
+    i.ondragover = (evt) => {
+      evt.preventDefault()
+    }
+
+    i.ondrop = (evt) => {
+      evt.preventDefault()
+
+      if (i !== current) {
+        let currentpos = 0
+        let droppedpos = 0
+
+        for (let it = 0; it < items.length; it++) {
+          if (current === items[it]) {
+            currentpos = it
+          }
+          if (i === items[it]) {
+            droppedpos = it
+          }
+        }
+
+        window.LPTE.emit({
+          meta: {
+            namespace: 'module-league-state',
+            type: 'swap-player'
+          },
+          currentpos,
+          droppedpos,
+          team: parseInt(i.dataset.team)
+        })
+
+        if (currentpos < droppedpos) {
+          i.parentNode.insertBefore(current, i.nextSibling)
+        } else {
+          i.parentNode.insertBefore(current, i)
+        }
+      }
+    }
+  }
 }
 
 window.LPTE.onready(() => {
