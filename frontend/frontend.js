e1e2c80a5b0d22ba27179248d46fc1aaccbbc7f5
Himyu <max_b98@hotmail.de>
2024-11-05 16:52:19 +0100

    implement new link methods in frontend

--- frontend/frontend.js
+++ frontend/frontend.js
@@ -1,6 +1,6 @@
 const updateUi = async () => {
-  const server = await window.constants.getWebServerPort()
-  const location = `http://${server}/pages/op-module-league-caster-cockpit/cockpit`
+  const server = await window.constants.getModuleURL()
+  const location = `${server}/cockpit`
 
   const apiKey = await window.constants.getApiKey()
 
